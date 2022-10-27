0d07f438583bfcafbf33ece32afb9d8e08cbe5d9
zue <zuleikatesei@gmail.com>
2022-10-27 13:13:38 -0700

    added reply schema and updated controllers and routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controllers');
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controllers');
 
 // /api/comments/<pizzaId>
-router.route('/:pizzaId').post(addComment);
+router
+    .route('/:pizzaId')
+    .post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 module.exports = router; 
