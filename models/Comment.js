50c5b129bbe3b4a52ad405069466a5598bd6f0e2
zue <zuleikatesei@gmail.com>
2022-10-27 20:09:52 -0700

    add validation to comment model and reply schema

--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,13 @@ const ReplySchema = new Schema(
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: true
         },
         createdAt: {
             type: Date,
